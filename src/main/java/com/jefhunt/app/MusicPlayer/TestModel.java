767f1deb6739bea71bb3eb62d2139850be9bbec6
jefhunt <umesh1401056@gmail.com>
2019-05-12 23:54:24 +0530

    wip

---
+++ src/main/java/com/jefhunt/app/MusicPlayer/TestModel.java
@@ -0,0 +1,14 @@
+package com.jefhunt.app.MusicPlayer;
+
+public class TestModel {
+
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
